45fe039de2c42ca1a4e8c5d0e52e5729ae47474e
firestso2 <firestso2@gmail.com>
2026-05-15 18:58:55 +0300

    config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ ORDER_START_NUM = 237
 
 SUPPORT_CONTACT = "@vorache777"
 SHOP_NAME = "Vоrache's Store"
-WELCOME_PHOTO = UDBY3950.png
+WELCOME_PHOTO = "UDBY3950.png"
 
 # Товары с очередью верификации (не банки)
 QUEUE_ITEMS_CATS = ["crypto", "bookmakers"]
